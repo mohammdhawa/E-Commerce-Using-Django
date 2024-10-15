257ff43581729539da9b857e24245058383eb4ed
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2024-10-15 11:03:23 +0300

    create views for product brand

---
+++ dummy_data.py
@@ -0,0 +1,17 @@
+# import os
+# import django
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+# django.setup()
+#
+# from faker import Faker
+# import random
+# from products.models import (Product, ProductImages, Brand, Review)
+# from django.contrib.auth.models import User
+#
+#
+# def seed_users(n):
+#     for _ in range(n):
+#         User.objects.create(
+#             username=
+#         )
--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Product, Brand, Review
+
+
+class ProductListView(ListView):
+    model = Product
+
+
+class ProductDetailView(DetailView):
+    model = Product
+
+
+class BrandListView(ListView):
+    model = Brand
+
+
+class BrandDetailView(DetailView):
+    model = Brand
