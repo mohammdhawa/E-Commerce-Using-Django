7f39368891098ccd839e10f1313394dfd624daf6
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2024-10-20 17:30:48 +0300

    create address model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+ADDRESS_CHOICES = (
+    ('Home', 'Home'),
+    ('Office', 'Office'),
+    ('Work', 'Work'),
+    ('Other', 'Other'),
+)
+
+class Address(models.Model):
+    user = models.ForeignKey(User, related_name='user_address', on_delete=models.CASCADE)
+    type = models.CharField(max_length=10, choices=ADDRESS_CHOICES)
+    address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.user)
