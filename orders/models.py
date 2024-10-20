470eed41244f3feb99438c4417bf18f13fd1969e
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2024-10-20 17:58:43 +0300

    design Order, OrderDetail, Coupon models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
+from utils.generate_code import generate_code
+from django.utils import timezone
 
-# Create your models here.
+
+ORDER_STATUS_CHOICES = (
+    ('Recieved', 'Recieved'),
+    ('Processing', 'Processing'),
+    ('Shipped', 'Shipped'),
+    ('Delivered', 'Delivered'),
+)
+class Order(models.Model):
+    user = models.ForeignKey(User, related_name='user_orders', on_delete=models.SET_NULL, null=True, blank=True)
+    status = models.CharField(max_length=10, choices=ORDER_STATUS_CHOICES, default='Delivered')
+    code = models.CharField(max_length=10, default=generate_code)
+    order_time = models.DateTimeField(default=timezone.now)
+    delivery_time = models.DateTimeField(null=True, blank=True)
+    coupon = models.ForeignKey('Coupon', related_name='order_coupon', on_delete=models.SET_NULL,
+                               null=True, blank=True)
+    total = models.FloatField()
+    total_with_coupon = models.FloatField(null=True, blank=True)
+
+
+class OrderDetail(models.Model):
+    order = models.ForeignKey(Order, related_name='order_details', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_detail_product', on_delete=models.SET_NULL,
+                                null=True, blank=True)
+    quantity = models.PositiveIntegerField()
+    price = models.FloatField()
+    total = models.FloatField()
+
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=20)
+    start_date = models.DateTimeField(default=timezone.now)
+    end_date = models.DateTimeField()
+    quantity = models.PositiveIntegerField()
+    discount = models.FloatField()
+
+    def save(self, *args, **kwargs):
+        if self.end_date is None:
+            week = self.start_date + timezone.timedelta(days=7)
+            self.end_date = week
+        super(Coupon, self).save(*args, **kwargs)
---
+++ utils/generate_code.py
@@ -0,0 +1,9 @@
+import string
+import random
+
+
+# This function is for generating code for every order
+def generate_code(n=8):
+    text = string.digits + string.ascii_uppercase + string.digits
+    code = ''.join([random.choice(text) for _ in range(n)])
+    return code
